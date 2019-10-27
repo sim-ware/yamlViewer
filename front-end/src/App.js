474b5595f1a569b97aa202abb12b8c264b44f192
Samir Gossain <samir.gossain@gmail.com>
2019-10-27 16:56:54 +0000

    Adds basic scaffold to FrontEnd

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  // URL Entry Screen
+  // // // // // // //
+  // return (
+  //   <div className="App">
+  //     <p className="Text">YAML Viewer</p>
+  //     <p className="Text">
+  //       please enter the GitHub URL of the YAML file you would like to view
+  //     </p>
+  //     <form>
+  //       <input type="text" name="name" />
+  //       <input type="submit" value="view" />
+  //     </form>
+  //   </div>
+  // );
+
+  // YAML View Screen
+  // // // // // // //
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="Text">
-          Hello World
-        </p>
-      </header>
+      <p className="Text">YAML Viewer</p>
+      <p className="Text">Viewing YAML: put request URL here</p>
+      <p className="Text">put ul, li here</p>
+      <p className="Text">view new YAML</p>
     </div>
   );
 }
